0393af3f2ef36f5d2effd42582b8cbc5f93764f6
Yuval Steinhart <yuval@cs.ucsb.edu>
2022-11-03 13:43:49 -0700

    Added course CRUD

---
+++ app/controllers/courses_controller.rb
@@ -0,0 +1,80 @@
+class CoursesController < AuthenticatedController
+    before_action :check_id
+    before_action :set_course, only: %i[ show edit update destroy ]
+  
+    # GET /posts or /posts.json
+    def index
+      @courses = Course.all
+    end
+  
+    def show
+      @roles = Role.where(course_id: @course.id)
+    end
+  
+    def new
+      @course = Course.new
+    end
+  
+    def edit
+    end
+  
+    def create
+      @course = Course.new(course_params)
+      @course.roles.build(user: @current_user, course: @course)
+      # @course.users << User.find(@current_user.id)
+
+  
+      respond_to do |format|
+        if @course.save
+          format.html { redirect_to course_url(@course), notice: "Course was successfully created." }
+          format.json { render :show, status: :created, location: @course }
+        else
+          format.html { render :new, status: :unprocessable_entity }
+          format.json { render json: @course.errors, status: :unprocessable_entity }
+        end
+      end
+    end
+  
+    def update
+      respond_to do |format|
+        if @course.update(course_params)
+          format.html { redirect_to course_url(@course), notice: "Course was successfully updated." }
+          format.json { render :show, status: :ok, location: @course }
+        else
+          format.html { render :edit, status: :unprocessable_entity }
+          format.json { render json: @course.errors, status: :unprocessable_entity }
+        end
+      end
+    end
+  
+    def destroy
+      roles = Role.where(course_id: @course.id)
+      roles.each do |role|
+        role.destroy
+      end
+  
+      @course.destroy
+  
+      respond_to do |format|
+        format.html { redirect_to courses_url, notice: "Course was successfully destroyed." }
+        format.json { head :no_content }
+      end
+    end
+  
+    private
+  
+      def check_id
+        return render_not_found unless (!params[:id] or Course.find_by(id: params[:id]))
+      end
+    
+    # Use callbacks to share common setup or constraints between actions.
+      def set_course
+        @course = Course.find(params[:id])
+      end
+  
+      # Only allow a list of trusted parameters through.
+      def course_params
+        params.require(:course).permit(:name)
+      end
+  end
+  
