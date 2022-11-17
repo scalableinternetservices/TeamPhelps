f2c2d34440b61b82a7122043fd428744a44db6a5
Yuval Steinhart <yuval@cs.ucsb.edu>
2022-11-17 14:01:21 -0800

    Removed authenticity token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
-    protect_from_forgery unless: -> { request.format.json? }
-    
-    def render_not_found
-        render :file => "#{Rails.root}/public/404.html",  :status => 404
-      end
+skip_before_action :verify_authenticity_token
+
+def render_not_found
+    render :file => "#{Rails.root}/public/404.html",  :status => 404
+    end
 end
