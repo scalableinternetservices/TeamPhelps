56fa832445ba619c0617842adb727a048da5837f
drholmie <animesh.leo@gmail.com>
2022-11-24 16:10:58 -0800

    Added scenarios and test lifecycle functions to serial_make_post
    - Eventually will combine all tests under one script for testing scenarios for multiple pages
    - Doing so will prevent code redundancy, but make things somewhat monolithic in return

--- k6tests/serial_make_post.js
+++ k6tests/serial_make_post.js
@@ -3,12 +3,19 @@ import { check, group, sleep } from "k6";
 import { Counter, Rate, Trend } from "k6/metrics";
 import exec from 'k6/execution';
 
-// const loginData = {"users":[{name:"users1", email:"users1@email.com"}]};
-
 export let options = {
-    stages: [
-        { target: 1, iterations: 1 }, //in 30s scale up to 10 virtual users
-    ],
+    scenarios:{
+        make_post:{
+            executor: 'ramping-vus',
+            exec: 'post',
+            startVUs: 0,
+            stages: [
+                { duration:'5s', target: 5},
+                { duration:'2s', target: 0},
+            ],
+            gracefulRampDown: '1s',
+        }
+    },
     noCookiesReset: true,
 };
 
@@ -17,72 +24,81 @@ export let options = {
 let successful_req = new Counter("successful_req");
 let check_failure_rate = new Rate("check_failure_rate");
 let time_to_first_byte = new Trend("time_to_first_byte", true);
+let url = "http://localhost:3000";
+const params = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+};
+//setup function runs only once
+//logs in for the cookie setup
+//creates a course for the url to be passed down to the virtual users
+export function setup() {
+    const jar = http.cookieJar();
+    //Login step -----------
+    let payload = JSON.stringify({
+        email: 'users1@email.com',
+        name:'users1',
+    });
+
+    let res = http.post(url+"/login", payload, params);
+    let login_url = res.url;
+    let check_res = check(res, {
+        "200 requests": (r) => r.status >= 200 && r.status <300,
+    });
+    let cookie = jar.cookiesForURL(login_url);
+
+    if (check_res) {
+        successful_req.add(1);
+    }
+    check_failure_rate.add(!check_res, { page: "login" });
+    time_to_first_byte.add(res.timings.waiting, { ttfbURL: res.url });
+    // sleep(2);
+    //Create course step --------
+    payload = JSON.stringify({
+        name:'load_testing_course'
+    });
+
+    res = http.post(url+"/courses", payload, params);
+    check_res = check(res, {
+        "200 requests": (r) => r.status >= 200 && r.status <300,
+    });
+    let course_url = url
+    if (check_res) {
+        successful_req.add(1);
+        course_url = res.url;
+    }
+    check_failure_rate.add(!check_res, { page: "course" });
+    time_to_first_byte.add(res.timings.waiting, { ttfbURL: res.url });
+    return { curl: course_url, cook: cookie };
+}
 
 //makes the assumption that user exists and course exists
 //user is user1, email is user1@email.com
-//course is load_testing_course1
-export default function() {
-    group("make_post", function() {
-        //Login step -----------
-        const jar = http.cookieJar();
-        let url = "http://phelps.eba-rpinrm9x.us-west-2.elasticbeanstalk.com/";
-        let payload = JSON.stringify({
-            email: 'users1@email.com',
-            name:'users1',
-        });
-        
-        const params = {
-            headers: {
-              'Content-Type': 'application/json',
-            },
-          };
-        let res = http.post(url+"login", payload, params);
-        let check_res = check(res, {
-            "200 requests": (r) => r.status >= 200 && r.status <300,
-        });
-        if (check_res) {
-            successful_req.add(1);
-        }
-        check_failure_rate.add(!check_res, { page: "login" });
-        time_to_first_byte.add(res.timings.waiting, { ttfbURL: res.url });
-        // sleep(2);
-        //Create course step --------
-        payload = JSON.stringify({
-            name:'load_testing_course1'+exec.instance.iterationsCompleted
-        });
+//course is load_testing_course
+export function post(data) {
+    const jar = http.cookieJar();
+    jar.set(data.curl, "_app_session", data.cook._app_session);
+    let course_url = data.curl;
+    console.log(course_url);
+    const params = {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+    };
 
-        res = http.post(url+"courses", payload, params);
-        check_res = check(res, {
-            "200 requests": (r) => r.status >= 200 && r.status <300,
-        });
-        let course_url = url
-        if (check_res) {
-            successful_req.add(1);
-            course_url = res.url
-        }
-        // console.log(course_url + "actual "+res.url)
-        check_failure_rate.add(!check_res, { page: "course" });
-        time_to_first_byte.add(res.timings.waiting, { ttfbURL: res.url });
-        
-        // Create post step---------
-        if(course_url != url){
-            for(let i = 0 ; i < 100; i++){
-                // console.log("should've made post?"+course_url);
-                payload = JSON.stringify({
-                    title:'post'+i,
-                    body:'load_testing_post_body'
-                });
-
-                res = http.post(course_url+"/posts", payload, params);
-                check_res = check(res, {
-                    "200 requests": (r) => r.status >= 200 && r.status <300,
-                });
-                if (check_res) {
-                    successful_req.add(1);
-                }
-                check_failure_rate.add(!check_res, { page: "login" });
-                time_to_first_byte.add(res.timings.waiting, { ttfbURL: res.url });
-            }
-        }
+    let payload = JSON.stringify({
+        title:'post'+exec.instance.iterationsCompleted,
+        body:'load_testing_post_body'
     });
+
+    let res = http.post(course_url+"/posts", payload, params);
+    let check_res = check(res, {
+        "200 requests": (r) => r.status >= 200 && r.status <300,
+    });
+    if (check_res) {
+        successful_req.add(1);
+    }
+    check_failure_rate.add(!check_res, { page: "login" });
+    time_to_first_byte.add(res.timings.waiting, { ttfbURL: res.url });
 }
